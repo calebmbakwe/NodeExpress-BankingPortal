5f9cf283771618202be427c6049c9b0eb7c397c5
KayLee <caleboau2012@gmail.com>
2018-11-06 11:29:31 +0100

    Handling Form Data

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 // account data
 const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'UTF8');
@@ -31,6 +32,29 @@ app.get('/', (req, res) => {
 })
 .get('/profile', (req, res) => {
 	res.render('profile', { user: users[0] });
+})
+.get('/transfer', (req, res) => {
+	res.render('transfer');
+})
+.post('/transfer', (req, res) => {accounts[req.body.from].balance -= parseInt(req.body.amount);
+	accounts[req.body.to].balance += parseInt(req.body.amount);
+
+	let accountsJSON = JSON.stringify(accounts, null, 4);
+	fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'UTF8');
+
+	res.render('transfer', { message: 'Transfer Completed' });
+})
+.get('/payment', (req, res) => {
+	res.render('payment', { account: accounts.credit });
+})
+.post('/payment', (req, res) => {
+	accounts.credit.balance -= parseInt(req.body.amount);
+	accounts.credit.available += parseInt(req.body.amount);
+
+	let accountsJSON = JSON.stringify(accounts, null, 4);
+	fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'UTF8');
+
+	res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
 app.listen(3000, (err) => {
